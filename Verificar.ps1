23d5d248b4ed8dddc372344d8b75db046723afbe
agarciagalan <112832093+agarciagalan@users.noreply.github.com>
2022-09-05 10:17:55 +0200

    Actualización Verificar.ps1
    Al ser un proceso en segundo plano, no se necesita un write-host que se había utilizado para realizar pruebas.
    Se elimina una variable innecesaria llamada $nombre.

--- Verificar.ps1
+++ Verificar.ps1
@@ -1,9 +1,7 @@
 # script para verificar si los servicios indicados estan corriendo
 $servicios = <lista_servicios>
 foreach ($servicio in $servicios){
-    $nombre = $servicio
     $estado = (Get-Service $servicio | Select-Object status)
-    write-host $estado
     # Si el servicio indicado está apagado
     if ($estado -like "*Stopped}"){
         # Iniciar servicio
@@ -11,7 +9,7 @@ foreach ($servicio in $servicios){
         # Obtener fecha y hora de activacion
         $fechHora = Get-Date
         # Establecer contenido del log
-        $contenido = $fechHora, $nombre, "Servicio iniciado."
+        $contenido = $fechHora, $servicio, "Servicio iniciado."
         # añadir el contenido al archivo log
         Add-Content -Value $contenido -Path <Ruta_log>
     }
