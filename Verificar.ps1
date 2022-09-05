ded8b0b08ff358ee76fd48b062d351cdaed95779
agarciagalan <112832093+agarciagalan@users.noreply.github.com>
2022-09-05 12:13:45 +0200

    Actualización Verificar.ps1
    Tras la última actualización, la variable estado queda inservible y se elimina para mayor claridad.

--- Verificar.ps1
+++ Verificar.ps1
@@ -1,7 +1,6 @@
 # script para verificar si los servicios indicados estan corriendo
 $servicios = <lista_servicios>
 foreach ($servicio in $servicios){
-    $estado = (Get-Service $servicio | Select-Object status)
     # Si el servicio indicado está apagado
     if (Get-Service -Name $servicio | Where-Object {$_.Status -eq "Stopped"}){
         # Iniciar servicio
