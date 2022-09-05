3d9292e57053b7b66ece069dab7109671bf105cb
agarciagalan <112832093+agarciagalan@users.noreply.github.com>
2022-09-05 09:19:09 +0200

    Creación EjecutarVerificador.ps1
    Script encargado de ejecutar el verificador de servicios cada 60 segundos

---
+++ EjecutarVerificador.ps1
@@ -0,0 +1,13 @@
+# script para ejecutar el verificador cada minuto
+for(;;) {
+ try {
+  # ejecutar script de verificacion
+  <ruta_script_verificacion_ej_C:/scripts/Verificar.ps1>
+ }
+ catch {
+  # pasar $_ a un archivo log
+  Add-Content -Value $_ -Path <ruta_log>
+ }
+ # esperar un minuto
+ Start-Sleep 60
+}
