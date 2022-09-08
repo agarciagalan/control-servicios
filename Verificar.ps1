4faed8d94fab7b03d35c0f2d4ee0d7461b2d6c83
agarciagalan <112832093+agarciagalan@users.noreply.github.com>
2022-09-08 17:19:01 +0200

    Actualización Verificar.ps1
    Se han cambiado las condiciones y se ha añadido una ruta real temporal para preguntar a alvaro.

--- Verificar.ps1
+++ Verificar.ps1
@@ -1,13 +1,13 @@
 # script para verificar si los servicios indicados estan corriendo
-$servicios = <lista_servicios>
-foreach ($servicio in $servicios){
-    # Si el servicio indicado está apagado
-    if (Get-Service -Name $servicio | Where-Object {$_.Status -eq "Stopped"}){
+foreach ($servicio in Get-Service){
+    # Si los servicios que empiezan por DLM y EN BATCH v13 están apagados
+    if(($servicio.ServiceName -like "DLM*") -or ($servicio.ServiceName -eq "EN BATCH v13") -and ($servicio.Status -eq "Stopped"))
+    {
         # Iniciar servicio
         Start-Service $servicio
         # Obtener fecha y hora de activacion
         $fechHora = Get-Date
         # añadir el contenido al archivo log
-        Add-Content -Value $fechHora, $servicio, "Servicio iniciado.", "----------------" -Path <Ruta_log>
+        Add-Content -Value $fechHora, $servicio, "Servicio iniciado.", "----------------" -Path "D:\scripts\VerificarServicios\log.txt"
     }
 }
